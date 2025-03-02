94fe6baddae98fee1ef87c18502d706096bfe334
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-02 15:23:19 +0200

    part1

---
+++ modules/main.js
@@ -0,0 +1,7 @@
+import { add, subtract } from './modules/math.js';
+
+const resultAdd = add(5, 3);
+const resultSubtract = subtract(10, 4);
+
+console.log(`Addition Result: ${resultAdd}`);
+console.log(`Subtraction Result: ${resultSubtract}`);
